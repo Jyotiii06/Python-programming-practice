4e185a88c02fcb13a07d4dfa8ccd2d32774551ac
Jyoti Rawat <266243325+Jyotiii06@users.noreply.github.com>
2026-03-13 20:09:09 +0530

    Added a Python program of sum of non repeated no.
    Uploaded a Python program that prints sum of non duplicate numbers and if the numbers are repeating then they are ignored and not added.

---
+++ sum_of_non-repeated_no.py
@@ -0,0 +1,14 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=int(input("Entert third number"))
+sum1=0
+sum1=a+b+c
+print("Sum of three numbers is ",sum1)
+sum2=0
+if a!=b and a!=c:
+    sum2+=a
+if b!=a and b!=c:
+    sum2+=b
+if c!=a and c!=b:
+    sum2+=c
+print("Sum of non-duplicate number is",sum2)
