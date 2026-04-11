d1484399ca31d4caf3ef5b0590f9003056428e49
Jyoti Rawat <266243325+Jyotiii06@users.noreply.github.com>
2026-04-11 19:53:29 +0530

    Added chatbot with basic user input and responses

---
+++ ai_chatbot.py
@@ -0,0 +1,17 @@
+def chatbot():
+    print("Hello! I am a simple AI chatbot. Type 'bye' to exit.")
+
+    while True:
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("Bot: Hi there!")
+        elif user == "how are you":
+            print("Bot: I'm just code, but I'm doing great!")
+        elif user == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: I don't understand that yet.")
+
+chatbot()
